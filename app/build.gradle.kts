5821ac9560086847c083935d01bea5c0e77be2dd
aneira3 <111013784+aneira3@users.noreply.github.com>
2026-03-11 22:32:17 -0500

    Feature/active ride #32 (#2)
    * work in progress
    
    * work in progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
